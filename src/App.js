2c42d46b2eb2e48dba2d54584fe5bdacd97a1abf
Heather Godbey <51833484+heathergodbey@users.noreply.github.com>
2021-06-02 19:38:55 -0400

    formatting and notes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
-import React, { useRef, useEffect, useState } from "react";
-import "./App.css";
-import { select } from "d3";
+import React, { useRef, useEffect, useState } from 'react';
+import './App.css';
+import { select } from 'd3';
 
 function App() {
   const [data, setData] = useState([25, 30, 45, 60, 20]);
-  const svgRef = useRef();
+  const svgRef = useRef(); // reference obj
+  // console.log(svgRef);
+
+  // access svg dom element
   useEffect(() => {
-    const svg = select(svgRef.current);
+    // console.log(svgRef);
+    const svg = select(svgRef.current); // pass svg dom element to d3
     svg
-      .selectAll("circle")
-      .data(data)
-      .join("circle")
-      .attr("r", value => value)
-      .attr("cx", value => value * 2)
-      .attr("cy", value => value * 2)
-      .attr("stroke", "red");
+      // d3 methods
+      .selectAll('circle')
+      .data(data) // pass array
+      // console.log(svg.selectAll('circle').data(data));
+      .join('circle')
+      .attr('r', (value) => value)
+      .attr('cx', (value) => value * 2)
+      .attr('cy', (value) => value * 2)
+      .attr('stroke', 'red');
   }, [data]);
   return (
-    <React.Fragment>
+    <>
+      {/* pass ref to dom element */}
       <svg ref={svgRef}></svg>
       <br />
-      <button onClick={() => setData(data.map(value => value + 5))}>
+      <button onClick={() => setData(data.map((value) => value + 5))}>
         Update data
       </button>
-      <button onClick={() => setData(data.filter(value => value < 35))}>
+      <button onClick={() => setData(data.filter((value) => value < 35))}>
         Filter data
       </button>
-    </React.Fragment>
+    </>
   );
 }
 
